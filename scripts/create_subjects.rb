97d17522e86bebb1d78061aee6c3b2a0b7e73f85
Edward Paget <ed.paget@gmail.com>
2013-06-20 08:31:24 -0500

    Better subject build script

--- scripts/create_subjects.rb
+++ scripts/create_subjects.rb
@@ -7,13 +7,21 @@ s3 = AWS::S3.new
 bucket = s3.buckets['zooniverse-demo']
 
 subjects = File.open('manifest_wise.txt', 'a')
-metadata = File.open(ARGV[1], 'r').map do |line|
-  line.scan(/.*HIP\s([0-9]+)[a-z\s]*([-0-9]{0,3}\.[0-9]+)[a-z\s]*([-0-9]{0,3}\.[0-9]+)/)[0]
-end.reject { |i| i.nil? }
+if ARGV[2]
+  metadata = File.open(ARGV[2], 'r').map do |line|
+    line.scan(/.*HIP\s([0-9]+)[a-z\s]*([-0-9]{0,3}\.[0-9]+)[a-z\s]*([-0-9]{0,3}\.[0-9]+)/)[0]
+  end.reject { |i| i.nil? }
+else 
+  metadata = []
+end
 
 dir = ARGV[0]
 image_sets = Dir["#{dir}/*.png"].group_by do |image|
-  File.basename(image).scan(/^fc_HIP_([0-9]+).*\.png$/)[0][0]
+  if ARGV[1] == 'hip'
+    File.basename(image).scan(/^fc_HIP_([0-9]+).*\.png$/)[0][0]
+  else
+   File.basename(image).scan(/^fc_([0-9\.]+)\+([0-9\.]+)_.*.png$/)[0] 
+  end
 end
 
 image_sets.each do |hip_id, images|
@@ -24,7 +32,11 @@ image_sets.each do |hip_id, images|
   images_hash = Hash[surveys.zip image_urls]
   images_hash['standard'] = image_urls.first
 
-  coords = metadata.select{|m| m[0] == hip_id}[0].drop(1)
+  if metadata.empty?
+    coords = hip_id
+  else
+    coords = metadata.select{|m| m[0] == hip_id}[0].drop(1)
+  end
 
   subject = {
     _id: bson_id,
@@ -33,7 +45,7 @@ image_sets.each do |hip_id, images|
     metadata: {
       files: original_file_names,
       image_bands: surveys,
-      hip_id: hip_id
+      hip_id: if hip_id.kind_of? Array then nil else hip_id end
     }
   }
 
